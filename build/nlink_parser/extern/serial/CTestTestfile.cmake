36c596e8d13f981eeccea6cf710a041c407dd9a3
Delia-minjun <delia-minjun@outlook.com>
2026-05-12 09:22:11 +0800

    update

--- build/nlink_parser/extern/serial/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /root/uwb_ws/src/nlink_parser/extern/serial
-# Build directory: /root/uwb_ws/build/nlink_parser/extern/serial
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("tests")
--- build/uwb_read/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /root/uwb_ws/src/uwb_read
-# Build directory: /root/uwb_ws/build/uwb_read
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
